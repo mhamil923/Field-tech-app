cec7a3b2a7db198f52f9a007b4fb855cedb8bbea
Mark Hamilton <markhamilton@MacBook-Pro.local>
2025-09-05 09:45:31 -0500

    Fix entrypoint & Babel: root index.js with gesture-handler, add reanimated plugin; set main=index.js; add build scripts

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,9 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    // Order matters: expo-router first, reanimated LAST
-    plugins: ['expo-router/babel', 'react-native-reanimated/plugin'],
+    plugins: [
+      'expo-router/babel',
+      'react-native-reanimated/plugin' // must be last
+    ],
   };
 };
